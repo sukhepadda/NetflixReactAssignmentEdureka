9a06bc2c1c4a5145d71d199d88b24f14cbbc30a7
sukhepadda <paddasukhwantsingh@gmail.com>
2023-03-06 11:53:02 +0530

    footer changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/landingpage/HomePage"
 import MovieLists from "./components/movielistpage/MovieLists.js"
+import Footer from "./components/Footer";
 
 
 function App() {
@@ -12,6 +13,8 @@ function App() {
         <Route path="/" element={<HomePage/>} />
         <Route path="/lists" element={<MovieLists/>} />
       </Routes>
+      {<Footer/>}
+
     </Router>
    
     </div>
--- src/components/landingpage/HomePage.js
+++ src/components/landingpage/HomePage.js
@@ -7,6 +7,7 @@ import content2 from "./images/content2.jpg";
 import content3 from "./images/content3.png";
 import iconImg from "./images/icon.png";
 
+
 import "./HomePage.css";
 
 function HomePage() {
@@ -299,89 +300,6 @@ function HomePage() {
         </div>
       </section>
 
-      <footer>
-        <div className="container footerContainer">
-          <ul>
-            <li className="footerCall ps-2 pb-3">
-              Questions? Call <a href="#"> 000-800-919-1694</a>
-            </li>
-          </ul>
-          <div className="row">
-            <div className="col-md-3 col-4">
-              <ul>
-                <li>
-                  <a href="#">FAQ</a>
-                </li>
-                <li>
-                  <a href="#">Media Centre</a>
-                </li>
-                <li>
-                  <a href="#">Ways to Watch</a>
-                </li>
-                <li>
-                  <a href="#">Cookie Preferences</a>
-                </li>
-                <li>
-                  <a href="#">Speed Test</a>
-                </li>
-              </ul>
-            </div>
-            <div className="col-md-3 col-4">
-              <ul>
-                <li>
-                  <a href="#">Help Centre</a>
-                </li>
-                <li>
-                  <a href="#">Investor Relations</a>
-                </li>
-                <li>
-                  <a href="#">Terms of Use</a>
-                </li>
-                <li>
-                  <a href="#">Cookie Preferences</a>
-                </li>
-                <li>
-                  <a href="#">Legal Notices</a>
-                </li>
-              </ul>
-            </div>
-            <div className="col-md-3 col-4">
-              <ul>
-                <li>
-                  <a href="#">Account</a>
-                </li>
-                <li>
-                  <a href="#">Jobs</a>
-                </li>
-                <li>
-                  <a href="#">Privacy</a>
-                </li>
-                <li>
-                  <a href="#">Contact Us</a>
-                </li>
-                <li>
-                  <a href="#">Only on Netflix</a>
-                </li>
-              </ul>
-            </div>
-            <div className="row pb-4 footerDropdown">
-              <div className="col-lg-2 col-md-4 col-6">
-                <select
-                  className="form-select-menu2 mx-4 text-white"
-                  aria-label="Default select example"
-                >
-                  <option selected value="english">
-                    <span>&nbsp;English</span>
-                  </option>
-                  <option value="hindi">&nbsp;हिंदी</option>
-                </select>
-              </div>
-            </div>
-
-            <p className="footerCredits">Netflix India</p>
-          </div>
-        </div>
-      </footer>
     </div>
   );
 }
