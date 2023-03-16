e17054085cd588d8cd1a15a7d42ca4fe57b79974
sukhepadda <paddasukhwantsingh@gmail.com>
2023-03-16 21:49:34 +0530

    done

--- src/components/searchmoviepage/MovieBox.js
+++ src/components/searchmoviepage/MovieBox.js
@@ -1,70 +1,38 @@
-import React from "react";
-import searchapireq from "./searchapireq";
-import "./MovieBox.css";
-function MovieBox({
-  id,
-  title,
-  poster_path,
-  vote_average,
-  release_date,
-  overview,
-}) {
-  return (
-    <div className="card text-center bg-secondary mb-3">
-      <div className="card-body">
-        <img
-          className="card-img-top"
-          src={searchapireq.imgURL + poster_path}
-          alt=""
-        />
-        <div className="card-body">
-          <button
-            type="button"
-            className="btn btn-primary"
-            data-bs-toggle="modal"
-            data-bs-target="#exampleModal"
-          >
-            View More
-          </button>
-          <div
-            className="modal fade"
-            id="exampleModal"
-            tabindex="-1"
-            aria-labelledby="exampleModalLabel"
-            aria-hidden="true"
-          >
-            <div className="modal-dialog">
-              <div className="modal-content">
-                <div className="modal-header">
-                  <h1 className="modal-title fs-5" id="exampleModalLabel">
-                    {title}
-                  </h1>
-                  <button
-                    type="button"
-                    className="btn-close"
-                    data-bs-dismiss="modal"
-                    aria-label="Close"
-                  ></button>
-                </div>
-                <div className="modal-body">
-                  <img
-                    className="card-img-top modalImg"
-                    src={searchapireq.imgURL + poster_path}
-                    alt=""
-                  />
-                  <h3 className="bodyContent mt-2">{title}</h3>
-                  <h4 className="bodyContent">IMDB: {vote_average}</h4>
-                  <h5 className="bodyContent">Release Date: {release_date}</h5>
-                  <h5 className="bodyContent">Overview:</h5>
-                  <p className="bodyContent">{overview}</p>
-                </div>
+import { Modal,show,Button} from 'react-bootstrap';
+import React, {useState} from 'react';
+const API_IMG="https://image.tmdb.org/t/p/w500/";
+
+const MovieBox =({title, poster_path, vote_average, release_date, overview})=>{
+    
+    const [show, setShow]=useState(false);
+
+    const handleShow=()=>setShow(true);
+    const handleClose=()=>setShow(false);
+    
+    return(
+        <div className="card main-card-div text-center m-3">
+            <div className="card-body card-background">
+              <img className="card-img-top" src={API_IMG+poster_path} />
+              <div className="card-body">
+                  <button type="button" className="btn btn-dark" onClick={handleShow} >View More</button>
+                  <Modal show={show} onHide={handleClose}>
+                      <Modal.Header closeButton>
+                        <Modal.Title><h3>{title}</h3></Modal.Title>
+                      </Modal.Header>
+                      <Modal.Body>
+                      <img className="card-img-top" style={{width:'14rem'}}src={API_IMG+poster_path} />
+                    
+                      <h4>IMDb: {vote_average}</h4>
+                      <h5>Release Date: {release_date}</h5>
+                      <br></br>
+                      <h6>Overview</h6>
+                      <p>{overview}</p>
+                      </Modal.Body>
+                  </Modal>
               </div>
             </div>
-          </div>{" "}
         </div>
-      </div>
-    </div>
-  );
+    )
 }
 
 export default MovieBox;
--- src/components/searchmoviepage/MovieSearch.js
+++ src/components/searchmoviepage/MovieSearch.js
@@ -1,65 +1,99 @@
 import React, { useState, useEffect } from "react";
-import MovieBox from "./MovieBox";
-import searchapireq from "./searchapireq";
 import "./MovieSearch.css";
+import MovieBox from "./MovieBox";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Navbar,
+  Container,
+  Nav,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
 
-function MovieSearch() {
+const API_URL =
+  "https://api.themoviedb.org/3/movie/popular?api_key=2950f935c91571f9066f42ad4a94dda7";
+const API_SEARCH =
+  "https://api.themoviedb.org/3/search/movie?api_key=2950f935c91571f9066f42ad4a94dda7>>&query";
+function App() {
   const [movies, setMovies] = useState([]);
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
-    fetch(searchapireq.apiURL)
+    fetch(API_URL)
       .then((res) => res.json())
       .then((data) => {
-        // console.log(data.results);
+        console.log(data);
         setMovies(data.results);
       });
   }, []);
 
-  const searchMovieFunction = async (e) => {
-    // e.preventDefault();
-    console.log("searching");
-    try{
-      const url = `https://api.themoviedb.org/3/search/movie?api_key=2950f935c91571f9066f42ad4a94dda7&query`;
-      console.log(url);
-
-    }catch(err){
+  const searchMovie = async (e) => {
+    e.preventDefault();
+    console.log("Searching");
+    try {
+      const url = `https://api.themoviedb.org/3/search/movie?api_key=bcc4ff10c2939665232d75d8bf0ec093&query=${query}`;
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
+      setMovies(data.results);
+    } catch (e) {
+      console.log(e);
     }
   };
 
+  const changeHandler = (e) => {
+    setQuery(e.target.value);
+  };
   return (
     <>
-      <nav className="navbar-expand-lg bg-body-tertiary">
-        <div claclassNamess="container-fluid">
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <form
-              onSubmit={searchMovieFunction()}
-              className="d-flex"
-              role="search"
-            >
-              <input
-                className="form-control me-2"
+      <Navbar bg="dark" expand="lg" variant="dark">
+        <Container fluid>
+          <Navbar.Toggle aria-controls="navbarScroll"></Navbar.Toggle>
+          <img
+        className="nav_logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png"
+        alt="Netflix Logo"
+      />
+          <Navbar.Collapse id="nabarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-3"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            ></Nav>
+
+            <Form className="d-flex" onSubmit={searchMovie} autoComplete="off">
+              <FormControl
                 type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button className="btn btn-outline-success" type="submit">
+                placeholder="Movie Search"
+                className="me-2"
+                aria-label="search"
+                name="query"
+                value={query}
+                onChange={changeHandler}
+              ></FormControl>
+              <Button variant="danger" type="submit">
                 Search
-              </button>
-            </form>
+              </Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <div>
+        {movies.length > 0 ? (
+          <div className="container-searchpage">
+            <div className="grid">
+              {movies.map((movieReq) => (
+                <MovieBox key={movieReq.id} {...movieReq} />
+              ))}
+            </div>
           </div>
-        </div>
-      </nav>
-      <div className="main">
-        <div className="container-search">
-          <div className="grid">
-            {movies.map((movieReq) => {
-              return <MovieBox key={movieReq.id} {...movieReq} />;
-            })}
-          </div>
-        </div>
+        ) : (
+          <h2>Sorry !! No Movies Found</h2>
+        )}
       </div>
     </>
   );
 }
 
-export default MovieSearch;
+export default App;
